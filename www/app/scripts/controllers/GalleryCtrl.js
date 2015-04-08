20bc44e24d83bcaecee612057849b5027f433472
Éverton Inocêncio <hewerthomn@gmail.com>
2015-04-08 09:49:10 -0400

    adicionado listagem de problemas reportados

--- www/app/scripts/controllers/GalleryCtrl.js
+++ www/app/scripts/controllers/GalleryCtrl.js
@@ -5,8 +5,30 @@ function GalleryCtrl($scope)
 {
 	function _init()
 	{
-
-	}
+		$scope.issues = [
+		{
+			id: 1,
+			photo: "http://placehold.it/140x120",
+			category: { name: 'Pavimentação' },
+			comment: 'Lorem Ipsum dolor',
+			created_at: new Date('2015', '04', '08', '09', '46')
+		},
+		{
+			id: 2,
+			photo: "http://placehold.it/143x123",
+			category: { name: 'Iluminação Pública' },
+			comment: '',
+			created_at: new Date('2015', '04', '07')
+		},
+		{
+			id: 3,
+			photo: "http://placehold.it/144x124",
+			category: { name: 'Transporte Público' },
+			comment: 'Não existe ponto de ônibus!',
+			created_at: new Date('2015', '04', '06')
+		}
+		];
+	};
 
 	function _apply()
 	{
