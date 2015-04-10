613ebb5e1221c11b42d51678b4343f09d49a8674
Everton Inocencio <hewerthomn@gmail.com>
2015-04-10 09:27:11 -0400

    exibido problemas em mapa

--- www/app/scripts/controllers/HomeCtrl.js
+++ www/app/scripts/controllers/HomeCtrl.js
@@ -1,7 +1,7 @@
 /*
  * Home Controller
  */
-function HomeCtrl($scope, $window, Map)
+function HomeCtrl($scope, $window, Aqui, Map)
 {
 	function _init()
 	{
@@ -11,16 +11,52 @@ function HomeCtrl($scope, $window, Map)
 			lat: -1027313.6600097648
 		};
 
+		$scope.issues = [];
+
+		$scope.categories = Aqui.Category.getAll();
+
 		Map.init({
 			id: 'map',
 			offset: 90,
 			startZoom: $scope.zoom,
-			startLonlat: $scope.startLonlat
+			startLonlat: $scope.startLonlat,
+			onSelectPoint: _onSelectPoint
 		});
 
 		$scope.getPosition();
 
+		_loadIssues();
+
 		angular.element($window).bind('resize', function() { Map.fixMapHeight(); });
+	}
+
+	function _onSelectPoint(feature)
+	{
+		var point = feature.data;
+
+		console.log('point', point);
+	};
+
+	function _loadIssues()
+	{
+		var points = [];
+
+		$scope.issues = Aqui.Issue.getLasts();
+
+		angular.forEach($scope.issues, function(value, key) {
+			var category = Aqui.Category.get(value.category_id);
+
+			points.push({
+				id: value.id,
+				icon: category.icon,
+				lon: value.lonlat.lon,
+				lat: value.lonlat.lat,
+			});
+		});
+
+		var options = { transformTo: 'EPSG:4326' };
+
+		Map.addPoints(points, options);
 	}
 
 	$scope.getPosition = function()
@@ -37,4 +73,4 @@ function HomeCtrl($scope, $window, Map)
 
 angular
 	.module('app.controllers')
-	.controller('HomeCtrl', ['$scope', '$window', 'Map', HomeCtrl]);
+	.controller('HomeCtrl', ['$scope', '$window', 'Aqui', 'Map', HomeCtrl]);
--- www/app/scripts/services/AquiService.js
+++ www/app/scripts/services/AquiService.js
@@ -3,20 +3,31 @@
  */
 function AquiService($http)
 {
+	_categories = [
+		{ id: 1, name: 'Pavimentação', icon: 'img/categories/1.png' },
+		{ id: 2, name: 'Iluminação Pública', icon: 'img/categories/2.png' },
+		{ id: 3, name: 'Queimada Urbana', icon: 'img/categories/3.png' },
+		{ id: 4, name: 'Limpeza Urbana', icon: 'img/categories/4.png' },
+		{ id: 5, name: 'Transporte Público', icon: 'img/categories/5.png' },
+		{ id: 6, name: 'Sinalização de Trânsito', icon: 'img/categories/6.png' },
+	];
+
 	return {
 
 		Category:
 		{
+			get: function(id)
+			{
+				var category;
+				angular.forEach(_categories, function(value) {
+					if(value.id == id) category = value;
+				});
+				return category;
+			},
+
 			getAll: function()
 			{
-				return [
-					{ id: 1, name: 'Pavimentação', icon: 'img/categories/1.png' },
-					{ id: 2, name: 'Iluminação Pública', icon: 'img/categories/2.png' },
-					{ id: 3, name: 'Queimada Urbana', icon: 'img/categories/3.png' },
-					{ id: 4, name: 'Limpeza Urbana', icon: 'img/categories/4.png' },
-					{ id: 5, name: 'Transporte Público', icon: 'img/categories/5.png' },
-					{ id: 6, name: 'Sinalização de Trânsito', icon: 'img/categories/6.png' },
-				];
+				return _categories;
 			}
 		},
 
@@ -38,23 +49,38 @@ function AquiService($http)
 				return [{
 					id: 1,
 					photo: "http://placehold.it/140x120",
+					lonlat: { lon: -63.89999999999999, lat: -8.766699999999993 },
 					category: { name: 'Pavimentação' },
+					category_id: 1,
 					comment: 'Lorem Ipsum dolor',
 					created_at: new Date('2015', '04', '08', '09', '46')
 				},
 				{
 					id: 2,
 					photo: "http://placehold.it/143x123",
+					lonlat: { lon: -63.87768402099619, lat: -8.749055369870185 },
 					category: { name: 'Iluminação Pública' },
+					category_id: 2,
 					comment: '',
 					created_at: new Date('2015', '04', '07')
 				},
 				{
 					id: 3,
 					photo: "http://placehold.it/144x124",
+					lonlat: { lon: -63.86292114257822, lat: -8.787227793836935 },
 					category: { name: 'Transporte Público' },
+					category_id: 5,
 					comment: 'Não existe ponto de ônibus!',
 					created_at: new Date('2015', '04', '06')
+				},
+				{
+					id: 4,
+					photo: "http://placehold.it/140x120",
+					lonlat: { lon: -63.90291824340827, lat: -8.752957619767722 },
+					category: { name: 'Queimada Urbana' },
+					category_id: 3,
+					comment: 'Lorem Ipsum dolor',
+					created_at: new Date('2015', '04', '08', '09', '46')
 				}];
 			},
 
