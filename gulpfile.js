3012d323c37ed329d0721e3da657c4b80e91f031
Randson Oliveira <randsonjs@gmail.com>
2015-04-12 12:13:13 -0400

    Add concat task

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,38 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var bower = require('bower');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var sh = require('shelljs');
+var gulp = require('gulp'),
+		gutil = require('gulp-util'),
+		concat = require('gulp-concat');
 
-var paths = {
-  sass: ['./scss/**/*.scss']
-};
+gulp.task('default', function() {
 
-gulp.task('default', ['sass']);
-
-gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
 });
 
-gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['sass']);
-});
+gulp.task('concat', function() {
+	return gulp.src([
+		'www/lib/angular/angular.js',
+		'www/lib/angular-animate/angular-animate.js',
+		'www/lib/angular-sanitize/angular-sanitize.js',
+		'www/lib/angular-touch/angular-touch.js',
+		'www/lib/angular-focus-it/angular-focus-it.js',
+		'www/lib/angular-ui-router/release/angular-ui-router.js',
+		'www/lib/ionic/js/ionic.js',
+		'www/lib/ionic/js/ionic-angular.js',
+		'www/lib/ngCordova/dist/ng-cordova.js',
 
-gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-    });
-});
+		'www/app/scripts/directives/main.js',
 
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
+		'www/app/scripts/controllers/main.js',
+		'www/app/scripts/controllers/AppCtrl.js',
+		'www/app/scripts/controllers/HomeCtrl.js',
+		'www/app/scripts/controllers/GalleryCtrl.js',
+
+		'www/app/scripts/services/main.js',
+		'www/app/scripts/services/AquiService.js',
+		'www/app/scripts/services/MapService.js',
+		'www/app/scripts/services/CameraService.js',
+		'www/app/scripts/services/GeocoderService.js',
+
+		'www/app/scripts/app.js'
+	])
+	.pipe(concat('app.min.js', {newLine: ';'}))
+	.pipe(gulp.dest('www/build/js/'));
 });
