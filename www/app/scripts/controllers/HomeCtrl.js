d3eaf68202fe4d9c7e51539e268b18c6333369c2
Éverton Inocêncio <hewerthomn@gmail.com>
2015-04-13 22:00:33 -0400

    corrigido obter dados de cidade

--- www/app/scripts/controllers/HomeCtrl.js
+++ www/app/scripts/controllers/HomeCtrl.js
@@ -1,15 +1,18 @@
 /*
  * Home Controller
  */
-function HomeCtrl($scope, $window, $state, Aqui, Map)
+function HomeCtrl($scope, $window, $state, Aqui, Map, Geocoder)
 {
 	function _init()
 	{
 		$scope.zoom = 4;
 		$scope.issues = [];
-		$scope.startLonlat = {
-			lon: -5801876.194150391,
-			lat: -1027313.6600097648
+		$scope.city = {
+			name: '...',
+			lonlat: {
+				lon: -5801876.194150391,
+				lat: -1027313.6600097648
+			}
 		};
 
 		Aqui.Category.getAll()
@@ -24,12 +27,13 @@ function HomeCtrl($scope, $window, $state, Aqui, Map)
 			id: 'map',
 			offset: 90,
 			startZoom: $scope.zoom,
-			startLonlat: $scope.startLonlat,
+			startLonlat: $scope.city.lonlat,
 			onSelectPoint: _onSelectPoint
 		});
 
 		$scope.getPosition();
 
+		_getCity();
 		_loadIssues();
 
 		angular.element($window).bind('resize', function() { Map.fixMapHeight(); });
@@ -70,6 +74,34 @@ function HomeCtrl($scope, $window, $state, Aqui, Map)
 		Map.addPoints(points, { transformTo: 'EPSG:4326' });
 	};
 
+	function _getCity()
+	{
+		Map.getPosition(function(lonlat) {
+			_getCityInfo(lonlat);
+		}, function(error) {
+			alert(error);
+			console.log(error);
+		});
+	};
+
+	function _getCityInfo(lonlat)
+	{
+		$scope.city.lonlat = lonlat;
+
+		Geocoder
+			.getPlaceInfo(lonlat)
+			.success(function(response) {
+				if(response.hasOwnProperty('results'))
+				{
+					$scope.city.name = response.results[0].address_components[4].long_name;
+				}
+			})
+			.error(function(error){
+				console.error(error);
+				alert(error);
+			});
+	};
+
 	$scope.getPosition = function()
 	{
 		Map.getPosition(function(lonlat) {
@@ -84,4 +116,4 @@ function HomeCtrl($scope, $window, $state, Aqui, Map)
 
 angular
 	.module('app.controllers')
-	.controller('HomeCtrl', ['$scope', '$window', '$state', 'Aqui', 'Map', HomeCtrl]);
+	.controller('HomeCtrl', ['$scope', '$window', '$state', 'Aqui', 'Map', 'Geocoder', HomeCtrl]);
