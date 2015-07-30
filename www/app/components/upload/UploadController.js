d6d52742d35d11954fc78cce6132ba64f80ea2b3
Éverton Inocêncio <hewerthomn@gmail.com>
2015-07-30 12:04:39 -0400

    unificado formatacao de codigo

--- www/app/components/upload/UploadController.js
+++ www/app/components/upload/UploadController.js
@@ -8,10 +8,9 @@
 		.module('app')
 		.controller('UploadController', UploadController);
 
-	function UploadController($scope, $state, $cordovaToast, Aqui, Category, Camera, Issue, Map)
-	{
-		function _init()
-		{
+	function UploadController($scope, $state, $cordovaToast, Aqui, Category, Camera, Issue, Map) {
+
+		function _init() {
 			Aqui.init();
 
 			$scope.issue = Issue.new();
@@ -22,16 +21,17 @@
 			Category.getAll()
 				.success(function(categories) {
 					$scope.categories = categories;
+				})
+				.error(function(err) {
+					alert(JSON.stringify(err));
 				});
 		}
 
-		function _apply()
-		{
+		function _apply() {
 			if(!$scope.$$phase) $scope.$apply();
 		}
 
-		function _getPosition()
-		{
+		function _getPosition() {
 			Map.getPosition(function(lonlat) {
 				$scope.issue.lonlat = lonlat;
 			}, function(error) {
@@ -39,10 +39,8 @@
 			});
 		}
 
-		function _takePhoto(issue)
-		{
+		function _takePhoto(issue) {
 			$scope.issue = issue || Issue.new();
-
 			Camera.getPicture(function(imageUri) {
 				_getPosition();
 				$scope.issue.photo = imageUri;
@@ -50,27 +48,23 @@
 			});
 		}
 
-		function _showToastError(error)
-		{
+		function _showToastError(error) {
 			var msg = 'Ops, algum erro ao reportar problema...\n';
 			if(error) msg += 'Error code: ' + error.responseCode + "\n" + error.response;
 			return msg;
 		}
 
-		function _send(issue)
-		{
+		function _send(issue) {
 			$scope.sending = true;
 			$scope.uploadProgress = 0;
 
 			Issue.save(issue, function(result) {
 				$scope.sending = false;
-				if(result.responseCode == 200)
-				{
+
+				if(result.responseCode == 200) {
 					$scope.issue = null;
 					$cordovaToast.showShortCenter(result.response);
-				}
-				else
-				{
+				} else {
 					$cordovaToast.showShortCenter(_showToastError(result));
 				}
 
@@ -78,14 +72,12 @@
 			}, function(error) {
 				$scope.sending = false;
 				_showToastError(error);
-
 			}, function(progress) {
 				$scope.uploadProgress = parseInt((progress.loaded / progress.total) * 100, 10);
 			});
 		}
 
-		$scope.icon = function(category_id)
-		{
+		$scope.icon = function(category_id) {
 			if(category_id > 0)
 			{
 				var icon = '';
@@ -96,27 +88,18 @@
 			}
 		};
 
-		$scope.takePhoto = function(issue)
-		{
+		$scope.takePhoto = function(issue) {
 			_takePhoto(issue);
 		};
 
-		$scope.send = function(issue)
-		{
-			if(!$scope.$storage.user)
-			{
+		$scope.send = function(issue) {
+			if(!$scope.$storage.user) {
 				alert('Entre com a conta do Facebook');
-			}
-			else if(issue.category_id === 0)
-			{
+			} else if(issue.category_id === 0) {
 				alert('Selecione a categoria do problema');
-			}
-			else if(issue.photo === '' && $scope.photo !== 'img/camera.png')
-			{
+			} else if(issue.photo === '' && $scope.photo !== 'img/camera.png') {
 				alert('Toque na câmera para tirar foto');
-			}
-			else
-			{
+			} else {
 				_send(issue);
 			}
 		};
